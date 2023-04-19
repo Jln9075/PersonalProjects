6071c69a1863372c6aa3c20d5f3b4e3d9266f7c6
James Nelson <logannelson@yahoo.com>
2023-04-19 18:48:36 -0400

    working commit

---
+++ StatTrackerFrontEnd/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <header>
+          LeagueStatTracker
+      </header>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+header {
+  color:  black;
+  background-color: #92a8d1;
+  align-content: center;
+}
+
+</style>
